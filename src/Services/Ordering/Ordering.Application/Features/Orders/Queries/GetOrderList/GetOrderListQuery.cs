597fc3a6c6d9233ab25e56a9a9e8bd50623cc1cc
Skip <skoppensboer@gmail.com>
2021-05-05 08:41:49 +0200

    Created order query

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrderList/GetOrderListQuery.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrderList
+{
+    public class GetOrderListQuery : IRequest<List<OrderDTO>>
+    {
+        public GetOrderListQuery(string userName)
+        {
+            UserName = userName ?? throw new ArgumentNullException(nameof(userName));
+        }
+
+        public string UserName { get; set; }
+
+        
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrderList/GetOrderListQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrderList
+{
+    public class GetOrderListQueryHandler : IRequestHandler<GetOrderListQuery, List<OrderDTO>>
+    {
+        private readonly IOrderRepository _repository;
+        private readonly IMapper _mapper;
+
+        public GetOrderListQueryHandler(IOrderRepository repository, IMapper mapper)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        public async Task<List<OrderDTO>> Handle(GetOrderListQuery request, CancellationToken cancellationToken)
+        {
+            var orderlist = await _repository.GetOrdersByUserName(request.UserName);
+            return _mapper.Map<List<OrderDTO>>(orderlist);
+        }
+    }
+}
